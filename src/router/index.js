a6fca93162937aafcd1ec66dc464afd22816ff86
litus <litulandio@gmail.com>
2021-08-31 20:54:10 +0200

    initial project

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue')
+  },
+  
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>HOME</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+
+}
+</script>
+
+<style>
+
+</style>
