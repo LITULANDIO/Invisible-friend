42fc85f85fec56d31c5dcfeb65fe9fe55052ddc0
litus <litulandio@gmail.com>
2021-12-11 01:24:28 +0100

    fix wishes nadal

--- src/components/FriendNadal.vue
+++ src/components/FriendNadal.vue
@@ -124,7 +124,8 @@ export default {
             amicNadal,
             wishNadal,
             myWishesNadal,
-            createWishesNadal
+            createWishesNadal,
+            wishesNadal
 
         }
     }
